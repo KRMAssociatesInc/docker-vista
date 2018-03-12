153e5dae6942ee838ac74f2d98b2e8c7c2e6c3e9
Sam Habiel <Sam.Habiel@gmail.com>
2018-03-12 10:01:51 -0400

    Bug caused by previous commit:
    instance name should be CACHE in ccontrol start/stop

--- Cache/install.sh
+++ Cache/install.sh
@@ -137,7 +137,7 @@ mv /etc/redhat-release.orig /etc/redhat-release
 
 # create startup script used by docker
 echo "#!/bin/bash"                                      > $basedir/bin/start.sh
-echo 'trap "ccontrol stop '${instance}' quietly" SIGTERM' >> $basedir/bin/start.sh
+echo 'trap "ccontrol stop CACHE quietly" SIGTERM'       >> $basedir/bin/start.sh
 echo 'echo "Starting sshd"'                             >> $basedir/bin/start.sh
 echo "/usr/sbin/sshd"                                   >> $basedir/bin/start.sh
 echo 'echo "Starting vista processes"'                  >> $basedir/bin/start.sh
@@ -145,7 +145,7 @@ echo 'cp '${basedir}'/cache.cpf '${basedir}'/cache.cpf-old' >> $basedir/bin/star
 echo 'rm '${basedir}'/cache.cpf_*'                      >> $basedir/bin/start.sh
 echo 'cp '${basedir}'/cache.cpf-new '${basedir}'/cache.cpf' >> $basedir/bin/start.sh
 echo 'find '${basedir}'/ -iname CACHE.DAT -exec touch {} \;' >>$basedir/bin/start.sh
-echo "ccontrol start ${instance}"                       >> $basedir/bin/start.sh
+echo "ccontrol start CACHE"                             >> $basedir/bin/start.sh
 echo '# Create a fifo so that bash can read from it to' >> $basedir/bin/start.sh
 echo '# catch signals from docker'                      >> $basedir/bin/start.sh
 echo 'rm -f ~/fifo'                                     >> $basedir/bin/start.sh
